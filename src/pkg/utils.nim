3157f131f6ea0ab9b0ab7a5762cfae138b0a0f3a
kerorinnf <kitakitsune.gingisune.8220A@gmail.com>
2023-06-13 11:47:05 +0900

    configファイル操作を実装

--- src/pkg/utils.nim
+++ src/pkg/utils.nim
@@ -2,7 +2,8 @@ import
   std/os,
   std/parsecfg,
   std/httpclient,
-  std/json
+  std/json,
+  std/strutils
 
 const
   CONFIGPATH*: string = "config.ini"
@@ -15,15 +16,53 @@ proc getLatestVersion*(url:string): string =
     res = clt.getContent(url)
   except OSError:
     return ""
-
   let j = res.parseJson()
   return j["name"].getStr()
 
 proc makeConfig*(path:string=""): void =
   var dict: Config = newConfig()
-  # TODO: コンフィグの内容記述
+  dict.setSectionKey("DEFAULT", "isFirstLaunch", "0")
+  dict.setSectionKey("LANGUAGE", "mode", "0")
+  dict.setSectionKey("PATH", "path", path)
   dict.writeConfig(CONFIGPATH)
 
 if not CONFIGPATH.fileExists:
   makeConfig()
 
+# config.iniの[DEFAULT]のisFirstLaunchの取得
+proc loadIsFirstLaunch*(): int =
+  var dict: Config = loadConfig(CONFIGPATH)
+  let isFirstLaunch: string = dict.getSectionValue("DEFAULT", "isFirstLaunch")
+  return isFirstLaunch.parseInt
+
+# config.iniの[DEFAULT]のisFirstBootのupdate
+proc updateIsFirstLaunch*(isFirstLaunch:int): void =
+  var dict: Config = loadConfig(CONFIGPATH)
+  dict.setSectionKey("DEFAULT", "isFirstLaunch", $isFirstLaunch)
+  dict.writeConfig(CONFIGPATH)
+
+# config.iniの[LANGUAGE]のmodeの取得
+proc loadLangMode(): int =
+  var dict: Config = loadConfig(CONFIGPATH)
+  let mode: string = dict.getSectionValue("LANGUAGE", "mode")
+  return mode.parseInt
+
+let LANGMODE*: int = loadLangMode()
+
+# config.iniの[LANGUAGE]のmodeのupdate
+proc updateLangMode(mode:int): int =
+  var dict: Config = loadConfig(CONFIGPATH)
+  dict.setSectionKey("LANGUAGE", "mode", $mode)
+  dict.writeConfig(CONFIGPATH)
+
+# config.iniの[PATH]のpathの取得
+proc loadPath(): string =
+  var dict: Config = loadConfig(CONFIGPATH)
+  let path = dict.getSectionValue("PATH", "path")
+  return path
+
+# config.iniの[PATH]のpathのupdate
+proc updatePath(path:string): void =
+  var dict: Config = loadConfig(CONFIGPATH)
+  dict.setSectionKey("PATH", "path", path)
+  dict.writeConfig(CONFIGPATH)
