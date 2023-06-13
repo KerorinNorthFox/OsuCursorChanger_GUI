c7be6cfc3cad9cfc25ad2499e19326e315897e29
kerorinnf <kitakitsune.gingisune.8220A@gmail.com>
2023-06-13 16:12:20 +0900

    他ウィンドウのコントロール実装

--- src/pkg/gui.nim
+++ src/pkg/gui.nim
@@ -28,6 +28,7 @@ type
     isSelectedSeq: seq[Skin] # スキンファイルのうち選択されたものを一つ追加
 
   ToolWindow = ref object of ContainsSkinfile
+    buttonUi: LayoutContainer
     changeButton: Button # 変更ボタン
     cancelButton: Button # キャンセルボタン
 
@@ -71,13 +72,12 @@ method createWindow(self:Application, title:string="", height:int=100, width:int
 method showWindow(self:Application): void {.base.}
 method setControls(self:Application): void {.base.} =
   self.ui = newLayoutContainer(Layout_Horizontal)
-  self.window.add(self.ui)
 
 ##################################
 # Implementation #################
 ##################################
 var
-  application: MainWindow = MainWindow(isShowed:false)
+  application: MainWindow = MainWindow()
 
 proc main(): void =
   app.init()
@@ -95,18 +95,23 @@ proc newApp(self:MainWindow): void =
   self.createWindow("OsuCursorChanger GUI - "&VERSION, 500, 800)
   let height: int = 500
   let width: int = 800
-  self.changeCursor = ChangeCursorWindow(isShowed:false)
+  self.changeCursor = ChangeCursorWindow()
   self.changeCursor.createWindow("Change Cursor", height, width)
-  self.changeHitSound = ChangeHitSoundWindow(isShowed:false)
+  self.changeHitSound = ChangeHitSoundWindow()
   self.changeHitSound.createWindow("Change HitSound", height, width)
-  self.addCursor = AddCursorWindow(isShowed:false)
+  self.addCursor = AddCursorWindow()
   self.addCursor.createWindow("Add Cursor", height, width)
-  self.addHitSound = AddHitSoundWindow(isShowed:false)
+  self.addHitSound = AddHitSoundWindow()
   self.addHitSound.createWindow("Add HitSound", height, width)
-  self.setting = SettingWindow(isShowed:false)
+  self.setting = SettingWindow()
   self.setting.createWindow("Settings", 300, 400)
 
   self.setControls()
+  self.changeCursor.setControls()
+  self.changeHitSound.setControls()
+  self.addCursor.setControls()
+  self.addHitSound.setControls()
+  self.setting.setControls()
 
 # ウィンドウ作成
 method createWindow(self:Application, title:string="", height:int=100, width:int=100): void =
@@ -127,7 +132,7 @@ method showWindow(self:Application): void =
 ##################################
 method setControls(self:MainWindow): void =
   self.ui = newLayoutContainer(Layout_Horizontal)
-  
+
   self.skinfileUi = newLayoutContainer(Layout_Vertical)
   self.rightUi = newLayoutContainer(Layout_Vertical)
   self.topUi = newLayoutContainer(Layout_Vertical)
@@ -148,7 +153,51 @@ method setControls(self:MainWindow): void =
   self.bottomUi.add(self.addCursorButton)
   self.bottomUi.add(self.addHitSoundButton)
   self.bottomUi.add(self.settingButton)
+  
+  self.isShowed = false
+  self.skinfiles = @[]
+  self.isSelectedSeq = @[]
 
+method setControls(self:ToolWindow): void =
+  self.ui = newLayoutContainer(Layout_Vertical)
+
+  self.skinfileUi = newLayoutContainer(Layout_Vertical)
+  self.buttonUi = newLayoutContainer(Layout_Horizontal)
+  self.changeButton = newButton("change")
+  self.cancelButton = newButton("cancel")
+
+  self.window.add(self.ui)
+  self.ui.add(self.skinfileUi)
+  self.ui.add(self.buttonUi)
+  self.buttonUi.add(self.changeButton)
+  self.buttonUi.add(self.cancelButton)
+
+  self.isShowed = false
+  self.skinfiles = @[]
+  self.isSelectedSeq = @[]
+
+method setControls(self:SettingWindow): void =
+  self.ui = newLayoutContainer(Layout_Vertical)
+  
+  self.changePathUi = newLayoutContainer(Layout_Vertical)
+  self.referDirUi = newLayoutContainer(Layout_Horizontal)
+  self.pathTextBox = newTextBox()
+  self.referDirButton = newButton("reference")
+  self.savePathButton = newButton("save")
+
+  self.changeLangUi = newLayoutContainer(Layout_Vertical)
+  self.comboBox = newComboBox()
+  self.saveLangButton = newButton("save")
+
+  self.window.add(self.ui)
+  self.ui.add(self.changePathUi)
+  self.changePathUi.add(self.referDirUi)
+  self.referDirUi.add(self.pathTextBox)
+  self.referDirUi.add(self.referDirButton)
+  self.changePathUi.add(self.savePathButton)
+  self.ui.add(self.changeLangUi)
+  self.changeLangUi.add(self.comboBox)
+  self.changeLangUi.add(self.saveLangButton)
 
 when isMainModule:
   main()
