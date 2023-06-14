ee4366c235796212d7453a64f21647e91a7aa056
kerorinnf <kitakitsune.gingisune.8220A@gmail.com>
2023-06-14 18:38:47 +0900

    ウィンドウのデザインを実装

--- src/pkg/gui.nim
+++ src/pkg/gui.nim
@@ -46,10 +46,12 @@ type
     referDirUi: LayoutContainer
     pathTextBox: TextBox
     referDirButton: Button
+    savePathUi: LayoutContainer
     savePathButton: Button
 
     changeLangUi: LayoutContainer # 言語変更UI
     comboBox: ComboBox
+    saveLangUi: LayoutContainer
     saveLangButton: Button
 
   MainWindow = ref object of ContainsSkinfile
@@ -86,6 +88,8 @@ proc main(): void =
   app.init()
   application.newApp()
   application.showWindow()
+  # application.changeCursor.showWindow()
+  application.setting.showWindow()
   # 初回起動の時の処理
   if loadIsFirstLaunch() == 0:
     var firstLaunch: FirstLaunchWindow = FirstLaunchWindow()
@@ -95,9 +99,10 @@ proc main(): void =
 
 # アプリケーション作成
 proc newApp(self:MainWindow): void =
-  self.createWindow("OsuCursorChanger GUI - "&VERSION, 500, 800)
-  let height: int = 500
-  let width: int = 800
+  self.createWindow("OsuCursorChanger GUI - "&VERSION, 500, 600)
+  let
+    height: int = 500
+    width: int = 500
   self.changeCursor = ChangeCursorWindow()
   self.changeCursor.createWindow("Change Cursor", height, width)
   self.changeHitSound = ChangeHitSoundWindow()
@@ -107,7 +112,7 @@ proc newApp(self:MainWindow): void =
   self.addHitSound = AddHitSoundWindow()
   self.addHitSound.createWindow("Add HitSound", height, width)
   self.setting = SettingWindow()
-  self.setting.createWindow("Settings", 300, 400)
+  self.setting.createWindow("Settings", 230, 400)
 
   self.setControls()
   self.changeCursor.setControls()
@@ -115,6 +120,8 @@ proc newApp(self:MainWindow): void =
   self.addCursor.setControls()
   self.addHitSound.setControls()
   self.setting.setControls()
+
+  self.setEvents()
 
 # ウィンドウ作成
 method createWindow(self:Application, title:string="", height:int=100, width:int=100): void =
@@ -134,17 +141,47 @@ method showWindow(self:Application): void =
 # Implementation - Controls ######
 ##################################
 method setControls(self:MainWindow): void =
+  const buttonHeight: int = 35
+
   self.ui = newLayoutContainer(Layout_Horizontal)
 
   self.skinfileUi = newLayoutContainer(Layout_Vertical)
+  self.skinfileUi.widthMode = WidthMode_Expand
+  self.skinfileUi.heightMode = HeightMode_Expand
+
   self.rightUi = newLayoutContainer(Layout_Vertical)
+  self.rightUi.heightMode = HeightMode_Expand
+  self.rightUi.width = 150
+  self.rightUi.xAlign = XAlign_Center
+
   self.topUi = newLayoutContainer(Layout_Vertical)
+  self.topUi.widthMode = WidthMode_Expand
+  self.topUi.frame = newFrame()
+
   self.changeCursorButton = newButton("Change Cursor")
+  self.changeCursorButton.widthMode = WidthMode_Expand
+  self.changeCursorButton.height = buttonHeight
+
   self.changeHitSoundButton = newButton("Change HitSound")
+  self.changeHitSoundButton.widthMode = WidthMode_Expand
+  self.changeHitSoundButton.height = buttonHeight
+
   self.bottomUi = newLayoutContainer(Layout_Vertical)
+  self.bottomUi.heightMode = HeightMode_Expand
+  self.bottomUi.widthMode = WidthMode_Expand
+  self.bottomUi.frame = newFrame("Tools")
+
   self.addCursorButton = newButton("Add Cursor")
+  self.addCursorButton.widthMode = WidthMode_Expand
+  self.addCursorButton.height = buttonHeight
+
   self.addHitSoundButton = newButton("Add HitSound")
+  self.addHitSoundButton.widthMode = WidthMode_Expand
+  self.addHitSoundButton.height = buttonHeight
+
   self.settingButton = newButton("Settings")
+  self.settingButton.widthMode = WidthMode_Expand
+  self.settingButton.height = buttonHeight
 
   self.window.add(self.ui)
   self.ui.add(self.skinfileUi)
@@ -161,13 +198,29 @@ method setControls(self:MainWindow): void =
   self.skinfiles = @[]
   self.isSelectedSeq = @[]
 
+
 method setControls(self:ToolWindow): void =
+  const buttonWidth: int = 100
+
   self.ui = newLayoutContainer(Layout_Vertical)
 
   self.skinfileUi = newLayoutContainer(Layout_Vertical)
+  self.skinfileUi.heightMode = HeightMode_Expand
+  self.skinfileUi.widthMode = WidthMode_Expand
+
   self.buttonUi = newLayoutContainer(Layout_Horizontal)
+  self.buttonUi.height = 60
+  self.buttonUi.widthMode = WidthMode_Expand
+  self.buttonUi.xAlign = XAlign_Right
+  self.buttonUi.padding = 12
+
   self.changeButton = newButton("change")
+  self.changeButton.heightMode = HeightMode_Expand
+  self.changeButton.width = buttonWidth
+
   self.cancelButton = newButton("cancel")
+  self.cancelButton.heightMode = HeightMode_Expand
+  self.cancelButton.width = buttonWidth
 
   self.window.add(self.ui)
   self.ui.add(self.skinfileUi)
@@ -183,24 +236,45 @@ method setControls(self:SettingWindow): void =
   self.ui = newLayoutContainer(Layout_Vertical)
   
   self.changePathUi = newLayoutContainer(Layout_Vertical)
+  self.changePathUi.widthMode = WidthMode_Expand
+  self.changePathUi.frame = newFrame("change osu! folder path")
+  
   self.referDirUi = newLayoutContainer(Layout_Horizontal)
-  self.pathTextBox = newTextBox()
+  self.referDirUi.widthMode = WidthMode_Expand
   self.referDirButton = newButton("reference")
+
+  self.pathTextBox = newTextBox()
+
+  self.savePathUi = newLayoutContainer(Layout_Horizontal)
+  self.savePathUi.widthMode = WidthMode_Expand
+  self.savePathUi.xAlign = XAlign_Right
+
   self.savePathButton = newButton("save")
 
   self.changeLangUi = newLayoutContainer(Layout_Vertical)
+  self.changeLangUi.widthMode = WidthMode_Expand
+  self.changeLangUi.frame = newFrame("change language")
+
   self.comboBox = newComboBox()
+  self.comboBox.widthMode = WidthMode_Expand
+
+  self.saveLangUi = newLayoutContainer(Layout_Horizontal)
+  self.saveLangUi.widthMode = WidthMode_Expand
+  self.saveLangUi.xAlign = XAlign_Right
+
   self.saveLangButton = newButton("save")
 
   self.window.add(self.ui)
   self.ui.add(self.changePathUi)
   self.changePathUi.add(self.referDirUi)
-  self.referDirUi.add(self.pathTextBox)
   self.referDirUi.add(self.referDirButton)
-  self.changePathUi.add(self.savePathButton)
+  self.referDirUi.add(self.pathTextBox)
+  self.changePathUi.add(self.savePathUi)
+  self.savePathUi.add(self.savePathButton)
   self.ui.add(self.changeLangUi)
   self.changeLangUi.add(self.comboBox)
-  self.changeLangUi.add(self.saveLangButton)
+  self.changeLangUi.add(self.saveLangUi)
+  self.saveLangUi.add(self.saveLangButton)
 
 ##################################
 # Implementation - Event Handler #
